c1ea144639333c0da3df24b6ed25c245fb23c741
Akshay Sanap <you@example.com>
2022-06-28 15:58:34 +0530

    Done added Success

---
+++ Array/src/main/java/com/csi/arraybasic/Arraybasic.java
@@ -0,0 +1,11 @@
+package com.csi.arraybasic;
+
+public class Arraybasic {
+	public static void main(String[] args) {
+		
+		String arr[]= {"A", "B","C","D","E","F",};
+		System.out.println(arr.length);
+
+	}
+
+}
